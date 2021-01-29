c65ed627c8bdb4724afc9adfbf84985b61115909
Peter <peter@pbwis.com>
2021-01-29 20:57:58 +0000

    Temporary test code - copy / wait

--- Forti_VPN_installer.ps1
+++ Forti_VPN_installer.ps1
@@ -8,3 +8,23 @@ $app.Uninstall()
 # Install new version
 
 Start-Process -FilePath "location to file.exe" -Verb runAs -ArgumentList '/s','/v"/qn"'
+
+
+Copy-Item -Path "Z:\path to file" -Destination "C:\!Test_PowerShell\path to folder" -Recurse
+
+
+Start-Process ((Resolve-Path "C:\!Test_PowerShell\folder3\Brzydula, odcinek 10, sezon 1.mp4").Path)
+
+$file = "C:\!Test_PowerShell\folder3\path to file"
+if (Test-Path -path $file)
+{
+
+    Start-Process ((Resolve-Path "C:\!Test_PowerShell\folder3\path to file").Path)
+}
+
+else
+{
+    return None
+}
+
+"notepad","calc","wmplayer", "explorer" | ForEach-Object {Start-Process $_} | Wait-Process ;dir
